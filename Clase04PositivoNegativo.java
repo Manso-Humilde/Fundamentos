0440ccdf419b58f86395d508b7b86fc487a09ca0
Manso-Humilde <edgar.morel.fp@gmail.com>
2025-06-04 09:53:40 +0200

    primer

---
+++ Clase04PositivoNegativo.java
@@ -0,0 +1,12 @@
+public class Clase04PositivoNegativo {
+    public static void main(String[] args) {
+        int numero = -3;
+
+        if (numero > 0) {
+            System.out.println("El número es POSITIVO");
+        }else{
+            System.out.println("El número es NEGATIVO");
+        }
+        System.out.println("Fin del Progrma");
+    }
+}
