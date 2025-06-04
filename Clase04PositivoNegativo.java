f2618dcb6f55a25d36bd58f5b06e4cc8c59551f8
Manso-Humilde <edgar.morel.fp@gmail.com>
2025-06-04 10:06:19 +0200

    tercero

--- Clase04PositivoNegativo.java
+++ Clase04PositivoNegativo.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Clase04PositivoNegativo {
     public static void main(String[] args) {
-        int numero = 0;
+        //conbinar con Scanner
+        Scanner teclado =  new Scanner(System.in);
 
+        System.out.println("Introducir número");
+
+        String dato = teclado.nextLine();
+
+        int numero = Integer.parseInt(dato);
         if (numero > 0) {
             System.out.println("El número es POSITIVO");
         }else if (numero == 0) {
