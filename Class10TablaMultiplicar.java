f83d68780e8d899619deb0743744fbae378fb6ec
Manso-Humilde <edgar.morel.fp@gmail.com>
2025-06-05 12:52:55 +0200

    segundo

--- Class10TablaMultiplicar.java
+++ Class10TablaMultiplicar.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 public class Class10TablaMultiplicar {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+       
+        System.out.println("Introduce un número: ");
         String dato = teclado.nextLine();
         
-        System.out.println("Introduce un número: ");
         int numero = Integer.parseInt(dato);
         
         for (int i = 1; i <= 10; i++) {
--- Class11Collatz.java
+++ Class11Collatz.java
@@ -1,3 +1,25 @@
+//Demostrar la conjetura Collatz
+
+import java.util.Scanner;
+
 public class Class11Collatz {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce un número: ");
+        String dato = teclado.nextLine();
+        int numero = Integer.parseInt(dato);
+
+        while (numero != 1) {
+            if (numero % 2 == 0) {
+                numero = numero / 2;
+            } else{
+                numero = numero * 3 + 1;
+            }
+            System.out.println(numero);
+        }
+
+    }
     
+
 }
