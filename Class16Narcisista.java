aacda244846041a3f05ccd02a483ac876744b47c
Manso-Humilde <edgar.morel.fp@gmail.com>
2025-06-06 21:02:54 +0200

    primero

--- Class16Narcisista.java
+++ Class16Narcisista.java
@@ -1,18 +1,26 @@
+
 import java.util.Scanner;
 
 public class Class16Narcisista {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Introduce un número");
+        System.out.println("Introduce un número:");
         String textoNumero = teclado.nextLine();
+        
+        int numeroOriginal = Integer.parseInt(textoNumero);
+        int longitud = textoNumero.length();
+        int suma = 0;
 
-        int  solucion = 0;
-
-        for (int i = 0; i < textoNumero.length(); i++){
-            
+        for (int i = 0; i < longitud; i++) {
+            int digito = Character.getNumericValue(textoNumero.charAt(i));
+            suma += Math.pow(digito, longitud); // elevar cada dígito a la cantidad de cifras
         }
 
-        int solucion = Math.pow(solucion, solucion)
+        if (suma == numeroOriginal) {
+            System.out.println("El número " + numeroOriginal + " es narcisista.");
+        } else {
+            System.out.println("El número " + numeroOriginal + " no es narcisista.");
+        }
 
     }
 }
