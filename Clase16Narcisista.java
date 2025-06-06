209d8e571818dce7000a69da65e7cd4129c22abd
Manso-Humilde <edgar.morel.fp@gmail.com>
2025-06-06 13:24:42 +0200

    primero

---
+++ Clase16Narcisista.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Clase16Narcisista {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce un número");
+        String textoNumero = teclado.nextLine();
+
+        int  solucion = 0;
+
+        for (int i = 0; i < textoNumero.length(); i++){
+            
+        }
+
+        int solucion = Math.pow(solucion, solucion)
+
+
+
+
+    }
+}
--- Class15SumarTextoNumeros.java
+++ Class15SumarTextoNumeros.java
@@ -3,7 +3,19 @@ import java.util.Scanner;
 public class Class15SumarTextoNumeros {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        String
+        System.out.println("Escriba un número");
+        String textoNumero = teclado.nextLine();
 
+        int suma = 0;
+
+        for(int i = 0; i < textoNumero.length(); i++){
+        //captura cada letra del texto
+        char letra = textoNumero.charAt(i);
+
+        String aux = String.valueOf(letra);
+        int numero = Integer.parseInt(aux);
+        suma += numero;
+    }
+    System.out.println("La suma de " + textoNumero + " es: " + suma);
     }
 }
